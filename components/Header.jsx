688b469e38ccb6d343e14f18b9f0f243cacf3352
darrenmfdes <darrenmfdes@gmail.com>
2022-02-27 23:51:33 +0530

    navbar 1st part complete

---
+++ components/Header.jsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="/assets/amazonLogoWhite.png"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+        {/* Search */}
+        <div className="m-2  hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2  h-full w-6 flex-shrink focus:outline-none rounded-l-md flex-grow"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+        {/* Right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello Darren Fernandes</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold------">
+              4
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold mt-2 md:text-sm">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Bottom nav */}
+      <div>
+        '
+        <p>
+          <MenuIcon />
+          All
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
